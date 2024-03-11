b8861457a68bcdc1e1f738ef63c7cec4452bd7a9
gmartin <gfmg1992@hotmail.com>
2024-03-11 09:47:40 +0000

    Week 7 udate

---
+++ Week 6/squareroot2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   squareroot.py
+@Time    :   2024/03/09 00:04:49
+@Author  :   Guillermo Martin
+@Version :   1.0
+@Personal email : gfmg1992@hotmail.com
+@Student email: G00438885@atu.ie
+@License :   (C)Copyright 2023, Guillermo Martin
+@Desc    :   Perform square root based on the Newton method
+Formula from the newton method found: 
+https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+'''
+
+def sqrt(l= 0.0001): # N: positive integer to calculate square root
+                        # l: the so called "tolerance limit". Default value 0.0001
+    N = False
+    while(N == False): 
+        try:
+            N = int(input("Enter the number you want the square root of: "))
+            if (N < 0):
+                print("The number must be postive: ") 
+        except ValueError:
+            print("That was not a number. Please enter a number: ")
+    
+        x = N #Define a new parameter x..As in newton formula
+
+        count = 0 #To keep track of the amount of loops to reach solution
+ 
+    while (1) : #Infinite loop until tolerace limit is achieve
+        count += 1 #Update loop count
+        print(f"Iteration {count}")
+ 
+        # Newton root formula
+        root = 0.5 * (x + (N / x)) 
+ 
+        #Once the difference between the root calculated value
+        # and x is less than the tolerance limit, 
+        # we finalize the function
+        if (abs(root - x) < l) : 
+            print(f"The square root of {N} is ~{round(root,2)}")
+            break
+ 
+        # If the above if statement is not achieved, we update the while loop 
+        x = root
+        
+    return root 
+    
+a=sqrt()    
