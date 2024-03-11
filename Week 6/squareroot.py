da9ec7635e8b27c86fe6da178172b980aba0d6e4
Guillermo Felipe Martin Gonzalez <53008413+gfmg1992@users.noreply.github.com>
2024-03-11 10:25:10 +0000

    Week 6 Update

--- Week 6/squareroot.py
+++ Week 6/squareroot.py
@@ -13,37 +13,41 @@ Formula from the newton method found:
 https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
 '''
 
-def sqrt(N, l= 0.0001): # N: positive integer to calculate square root
+def sqrt(l= 0.0001): # N: positive integer to calculate square root
                         # l: the so called "tolerance limit". Default value 0.0001
+    while True: 
+        try:
+            N = int(input("Enter the number you want the square root of: "))  
+            if N < 0: #Check the input is positive, otherwise ask for a number again
+                print("Please enter a non-negative number")
+                continue
+            break
+        except ValueError:
+            print("That was not a number. Please enter a number.")
     
-    # Firstly check N is an integer
-    if not isinstance(N,int):
-        print("Eror: The input must be an integer")
-        raise(2)
-    #If is an integer, check that is positive
-    elif N < 0:
-        print("Error: The number must be positive")
-        raise(2)
-    else: 
-        x = N #Define a new parameter x..As in newton formula
+    x = N #Define a new parameter x..As in newton formula
 
-        count = 0 #To keep track of the amount of loops to reach solution
+    count = 0 #To keep track of the amount of loops to reach solution
  
-        while (1) : #Infinite loop until tolerace limit is achieve
-            count += 1 #Update loop count
-            print(f"Iteration {count}")
- 
+    while (1) : #Infinite loop until tolerace limit is achieve
+        count += 1 #Update loop count
+        print(f"Iteration {count}")
+
+        if (N== 0): #Root squared of 0 is 0, Newton formula does not work
+            root=0
+        else:    
             # Newton root formula
             root = 0.5 * (x + (N / x)) 
  
-            #Once the difference between the root calculated value
-            # and x is less than the tolerance limit, 
-            # we finalize the function
-            if (abs(root - x) < l) : 
-                print(f"The square root of {N} is ~{round(root,2)}")
-                break
+        #Once the difference between the root calculated value
+        # and x is less than the tolerance limit, 
+        # we finalize the function
+        if (abs(root - x) < l) : 
+            print(f"The square root of {N} is ~{round(root,2)}")
+            return root 
+            break
  
-            # If the above if statement is not achieved, we update the while loop 
-            x = root
-        
-        return root 
+        # If the above if statement is not achieved, we update the while loop 
+        x = root
+    
+sqrt()    
